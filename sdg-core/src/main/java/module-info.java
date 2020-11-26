e62ab421d2ff67f65063231cc95fb1ab8d9b059d
David Olmos Garrido <daolgar3@inf.upv.es>
2020-11-26 23:10:21 +0100

    Add module-info.java to sdg-core

---
+++ sdg-core/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module sdg.core {
+  requires com.github.javaparser.core;
+  requires com.github.javaparser.symbolsolver.core;
+  requires org.jgrapht.core;
+  requires org.jgrapht.io;
+
+  exports es.upv.mist.slicing.slicing;
+  exports es.upv.mist.slicing.graphs;
+  exports es.upv.mist.slicing.graphs.sdg;
+  exports es.upv.mist.slicing.graphs.augmented;
+  exports es.upv.mist.slicing.graphs.pdg;
+  exports es.upv.mist.slicing.graphs.exceptionsensitive;
+}
